1894fce957a1adf2a5c6f6214c9c753257c02f47
Isabel Terenzi <bel.terenzi@gmail.com>
2022-09-10 15:52:47 -0300

    fix: tenta requisito 1 mais uma vez

--- src/server.js
+++ src/server.js
@@ -1,14 +1,14 @@
 const app = require('./app');
 require('dotenv').config();
-const connection = require('./connection');
+// const connection = require('./connection');
 
 // não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
 
 app.listen(process.env.PORT, async () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
 
-  const [result] = await connection.execute('SELECT 1');
-  if (result) {
-    console.log('MySQL connection is OK');
-  }
+ // const [result] = await connection.execute('SELECT 1');
+ // if (result) {
+  //  console.log('MySQL connection is OK');
+  // }
 });
