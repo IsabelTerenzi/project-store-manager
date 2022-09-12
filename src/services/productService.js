6f2dd3bddda6037f301c9a2d1b543c295e078c82
Isabel Terenzi <bel.terenzi@gmail.com>
2022-09-12 16:06:35 -0300

    requisito 4

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,4 +1,5 @@
 const productsModel = require('../models/productsModel');
+const productValidation = require('../middlewares/productValidation');
 
 const serviceGetAllProducts = async () => {
   const result = await productsModel.selectAllProducts();
@@ -11,6 +12,20 @@ const serviceGetProductsById = async (id) => {
 };
 
 const serviceCreateProduct = async ({ name }) => {
+  const { error } = productValidation.validate({ name });
+  const { details } = error;
+  if (details[0].type === 'string.min') {
+    throw Object.assign(
+      new Error(error.message),
+      { status: 422 },
+    );
+  }
+  if (details[0].type === 'any.required') {
+    throw Object.assign(
+      new Error(error.message),
+      { status: 400 },
+    );
+  }
   const id = await productsModel.insertProduct({ name });
   return { id, name };
 };
